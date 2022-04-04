600fca554b3f359d026fbc502bf3aa61c7e892cb
memegauste <memegauste@gmail.com>
2022-04-04 10:58:59 +0200

    Rememberize 0.2 alpha

--- rememberize.js
+++ rememberize.js
@@ -1,4 +1,4 @@
-// RememberizeJS 0.1 - don't forget formdata on reload.
+// RememberizeJS 0.2 - don't forget formdata on reload.
 // Requires cookiejs and jquery for now.
 
 const DJANGO_BLACKLIST = [
@@ -18,12 +18,12 @@ const miniCookieLib = {
             date.setTime(date.getTime() + (days*24*60*60*1000));
             expires = `; expires=${date.toUTCString()}`;
         }
-        document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+        document.cookie = `${name}=` + (value || "") + `${expires}; path=/`;
     },
     getCookie: (name) => {
-        let nameEQ = name + "=";
+        let nameEQ = `${name}=`;
         let ca = document.cookie.split(';');
-        for(let i=0;i < ca.length;i++) {
+        for(let i=0; i < ca.length; i++) {
             let c = ca[i];
             while (c.charAt(0) === ' ') c = c.substring(1,c.length);
             if (!c.indexOf(nameEQ)) return c.substring(nameEQ.length,c.length);
@@ -44,95 +44,101 @@ const miniCookieLib = {
     }
 };
 
-let rememberize = {
+const rememberize = {
     DEFAULT_EXPIRE_DAYS: 3,
     isCKeditor: (field) => {
-        let ID = field.attr('id');
-        let checkCKeditor = $(`#cke_${ID}`);
+        let ID = field.id;
+        let checkCKeditor = document.querySelectorAll(`#cke_${ID}`);
         return !!checkCKeditor.length;
     },
-    cleanFields: function(form){
+    cleanFields: function (form) {
         let inputs = form.find('input, select');
-        return inputs.filter(function() {
+        return inputs.filter(function () {
             let curr = this;
             let result = true;
+            let name = curr.getAttribute('name');
             DJANGO_BLACKLIST.forEach((entry) => {
-                if($(curr).attr('name') &&
-                    $(curr).attr('name').endsWith(entry)) result = false;
+                if (name && name.endsWith(entry)) result = false;
             });
             return result;
         });
     },
-    saveTextareas: function(form){
+    saveTextareas: function (form) {
         const self = this;
-        let textareas = form.find('textarea');
-        textareas.each(function(){
+        let textareas = form.querySelectorAll('textarea');
+        textareas.forEach((item) => {
             let val;
-            if(self.isCKeditor($(this))) val = CKEDITOR.instances[$(this).attr('id')].getData();
-            else val = $(this).val();
-            Cookies.set(
-                `rememberize[textarea]-${$(this).attr('id')}`, val, this.DEFAULT_EXPIRE_DAYS)
+            if (self.isCKeditor(item)) val = CKEDITOR.instances[item.id].getData();
+            else val = item.value;
+            miniCookieLib.setCookie(
+                `rememberize[textarea]-${item.id}`,
+                val, item.DEFAULT_EXPIRE_DAYS,
+            );
         });
     },
-    saveToCookie: function(form){
-        Cookies.set(`rememberize-${form.attr('id')}`,
-            this.cleanFields(form).serialize(), {expires: this.DEFAULT_EXPIRE_DAYS})
+    saveToCookie: function (form) {
+        miniCookieLib.setCookie(
+            `rememberize-${form.attr('id')}`,
+            this.cleanFields(form).serialize(),
+            this.DEFAULT_EXPIRE_DAYS,
+        );
         this.saveTextareas(form);
     },
-    initEvent: function(form){
+    initEvent: function (form) {
         const self = this;
-        form.find('input, select').on('change', () => {
-            self.saveToCookie(form);
+        console.log(form)
+        form.querySelectorAll('input, select').forEach((elem) => {
+            elem.addEventListener('change', () => {
+                self.saveToCookie(form);
+            }, false);
         });
-        form.find('textarea').each(function(){
-            if(self.isCKeditor($(this))){
-                CKEDITOR.instances[$(this).attr('id')].on('change', function(){
+        form.querySelectorAll('textarea').forEach((elem) => {
+            if (self.isCKeditor(elem)) {
+                CKEDITOR.instances[elem.id].addEventListener('change', () => {
                     self.saveTextareas(form);
-                })
-            } else $(this).on('change', () => {
+                }, false);
+            } else elem.addEventListener('change', () => {
                 self.saveTextareas(form);
             })
         })
     },
-    execute: function(selector){
+    execute: function (selector) {
         let self = this;
-        selector.each(function(){
-            if($(this).attr('id')) self.initEvent($(this));
+        selector.forEach((element) => {
+            if(element.id) self.initEvent(element);
         });
     },
-    loadToForm: function(id, serializedData){
+    loadToForm: function (id, serializedData) {
         $.each(serializedData.split('&'), (index, elem) => {
             let vals = elem.split('=');
-            let field = $(`#${id}`).find("[name='" + vals[0] + "']");
+            let field = document.querySelectorAll(`#${id} [name='${vals[0]}']`)[0];
             let value = decodeURIComponent(vals[1].replace(/\+/g, ' '));
-            if(field.attr('type') === 'checkbox'){
-                field.filter(function(){
-                    return this.value===value
+            if (field && field.tagName === 'checkbox') {
+                field.filter(function () {
+                    return this.value === value
                 }).prop('checked', true);
-            }
-            else field.val(value);
+            } else if(field) field.value = value;
         });
     },
-    formalize: function(){
+    formalize: function () {
         let self = this;
-        let allCookies = Cookies.get();
+        let allCookies = miniCookieLib.getAllCookies();
         Object.keys(allCookies).forEach(key => {
-            if(key.startsWith('rememberize-')){
+            if (key.startsWith('rememberize-')) {
                 this.loadToForm(key.replace('rememberize-', ''), allCookies[key])
-            }
-            else if(key.startsWith('rememberize[textarea]-')){
+            } else if (key.startsWith('rememberize[textarea]-')) {
                 let selector = $(`#${key.replace('rememberize[textarea]-', '')}`);
-                if(self.isCKeditor(selector)) CKEDITOR.instances[selector.attr('id')].setData(allCookies[key]);
-                else selector.val(allCookies[key]);
+                if (self.isCKeditor(selector)) CKEDITOR.instances[selector.attr('id')].setData(allCookies[key]);
+                else selector.value = allCookies[key];
             }
         })
     },
-    cleanUp: function(){
-        Object.keys(Cookies.get()).forEach(key => {
-            key.startsWith('rememberize') && Cookies.remove(key);
+    cleanUp: function () {
+        Object.keys(miniCookieLib.getAllCookies()).forEach(key => {
+            key.startsWith('rememberize') && miniCookieLib.removeCookie(key);
         });
     },
-    overallInit: function(selector){
+    overallInit: function (selector) {
         this.execute(selector);
         this.formalize();
     }
